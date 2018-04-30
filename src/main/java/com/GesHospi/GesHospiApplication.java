a255b8730cf32e4110b97420c035c1300f5198fa
popifra1409 <31097406+popifra1409@users.noreply.github.com>
2018-04-30 21:08:45 +0100

    initial commit

---
+++ src/main/java/com/GesHospi/GesHospiApplication.java
@@ -0,0 +1,12 @@
+package com.GesHospi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesHospiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesHospiApplication.class, args);
+	}
+}
